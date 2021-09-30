3979c4520da3ded1748e56a83d0823b50b4e9674
Boye <rippyblogger@gmail.com>
2021-09-30 17:15:41 +0400

    Add new component created

---
+++ src/components/AddNew.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <div class="ct-p-4 ct-text-black  ">
+            <button class="ct-p-4 ct-px-6 hover:ct-bg-green-200 ct-rounded ct-text-white">Add New</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "AddNew",
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+button{
+    background-color: #00B87C;
+}
+
+
+</style>
