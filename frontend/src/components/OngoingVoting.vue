29b93bc59694b9dc1948112df53a0dd33accf815
unknown <izik4004>
2021-09-30 22:12:47 +0100

    ongoing-voting

---
+++ frontend/src/components/OngoingVoting.vue
@@ -0,0 +1,94 @@
+<template>
+    <div class="ct-w-full ct-mx-auto">
+      <tabs>
+          <ul class="ct-flex ct-p-4 ct-cursor-pointer ct-gap-6">
+            <li class="ct-font-bold">Ongoing Voting</li>
+            <li>Requested Features</li>
+            <li>Admin</li>
+          </ul>
+       </tabs>
+       <div class="ct-flex ct-p-2 ct-justify-between ct-bg-green-100">
+           <div>Tickets added for voting</div>
+            <div>Votes</div>
+       </div>
+        <div class="ct-flex ct-p-2 ct-justify-between ct-border-b">
+            <div class="ct-justify-around">
+                <h3 class="ct-font-bold text-md">CreateTeamListView1</h3>
+                <p class="ct-text-gray-500 ct-text-sm">#001 added by John Doe</p>
+           </div> 
+            <div>
+                <label class="ct-px-2 ct-mt-2 ct-py-1 ct-rounded-xl ct-items-center ct-inline-flex ct-bg-gray-200">
+                    <input type="checkbox" class="ct-bg-green-600 ct-rounded-md ct-h-3 ct-w-3 " checked><span class="ct-text-black ct-ml-1 ct-text-sm">3</span>
+                </label>
+            </div>
+       </div>
+       <div class="ct-flex ct-p-2 ct-justify-between ct-border-b">
+            <div class="ct-justify-around">
+                <h3 class="ct-font-bold text-md">CreateTeamListView1</h3>
+                <p class="ct-text-gray-500 ct-text-sm">#001 added by John Doe</p>
+           </div> 
+            <div>
+                <label class="ct-px-2 ct-mt-2 ct-py-1 ct-rounded-xl ct-items-center ct-inline-flex ct-bg-gray-200">
+                    <input type="checkbox" class="ct-bg-green-600 ct-rounded-md ct-h-3 ct-w-3 " checked><span class="ct-text-black ct-ml-1 ct-text-sm">3</span>
+                </label>
+            </div>
+       </div>
+       <div class="ct-flex ct-p-2 ct-justify-between ct-border-b">
+            <div class="ct-justify-around">
+                <h3 class="ct-font-bold text-md">CreateTeamListView1</h3>
+                <p class="ct-text-gray-500 ct-text-sm">#001 added by John Doe</p>
+           </div> 
+            <div>
+                <label class="ct-px-2 ct-mt-2 ct-py-1 ct-rounded-xl ct-items-center ct-inline-flex ct-bg-gray-200">
+                    <input type="checkbox" class="ct-bg-green-600 ct-rounded-md ct-h-3 ct-w-3 " checked><span class="ct-text-black ct-ml-1 ct-text-sm">3</span>
+                </label>
+            </div>
+       </div>
+       <div class="ct-flex ct-p-2 ct-justify-between ct-border-b">
+            <div class="ct-justify-around">
+                <h3 class="ct-font-bold text-md">CreateTeamListView1</h3>
+                <p class="ct-text-gray-500 ct-text-sm">#001 added by John Doe</p>
+           </div> 
+            <div>
+                <label class="ct-px-2 ct-mt-2 ct-py-1 ct-rounded-xl ct-items-center ct-inline-flex ct-bg-gray-200">
+                    <input type="checkbox" class="ct-bg-green-600 ct-rounded-md ct-h-3 ct-w-3 " checked><span class="ct-text-black ct-ml-1 ct-text-sm">3</span>
+                </label>
+            </div>
+       </div>
+       <div class="ct-flex ct-p-2 ct-justify-between ct-border-b">
+            <div class="ct-justify-around">
+                <h3 class="ct-font-bold text-md">CreateTeamListView1</h3>
+                <p class="ct-text-gray-500 ct-text-sm">#001 added by John Doe</p>
+           </div> 
+            <div>
+                <label class="ct-px-2 ct-mt-2 ct-py-1 ct-rounded-xl ct-items-center ct-inline-flex ct-bg-gray-200">
+                    <input type="checkbox" class="ct-bg-green-600 ct-rounded-md ct-h-3 ct-w-3 " checked><span class="ct-text-black ct-ml-1 ct-text-sm">3</span>
+                </label>
+            </div>
+       </div>
+       <div class="ct-flex ct-p-4 ct-justify-between ct-bg-green-100">
+           <div>Archived ></div>
+       </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "OngoingVote",
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+button{
+    background-color: #00B87C;
+}
+
+
+</style>
