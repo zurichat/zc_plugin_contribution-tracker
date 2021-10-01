727b11fed56cfa4106f944e8cea0bdb18de82737
Mogakamo <mogaka.amo254@gmail.com>
2021-10-01 21:14:13 +0000

    commit

--- frontend/src/components/Comments.vue
+++ frontend/src/components/Comments.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
-      <p><b class="ct-ml-1">Comments</b>(Voting ends 10 minutes after the first comment)<b class="ct-float-right ct-mr-3">Votes</b></p>
-      <div class="ct-bg-gray-100 ct-block ct-p-px ct-py-1 ct-text-lg ct-font-light ct-my-1 ct-flex ct-space-x-60" v-for="comment in comments" :key="comment.id">
+      <p class=""><b class="ct-text-light ct-ml-1">Comments</b>(Voting ends 10 minutes after the first comment)<b class="ct-float-right ct-mr-3">Votes</b></p>
+      <div class="ct-bg-gray-100 ct-justify-between ct-pl-2 ct-block ct-p-px ct-py-1 ct-text-lg ct-font-light ct-my-1 ct-flex ct-space-x-60" v-for="comment in comments" :key="comment.id">
         {{comment.name}}
         <div class="ct-flex ct-float-right ct-mr-3">
-          <div class="ct-flex ct-items-center ct-h-5 ct-px-2 ct-ml-2 ct-bg-brand-gray-light-2 ct-space-x-1 ct-rounded-xl">
+          <div class="ct-flex ct-items-center ct-h-5 ct-px-2 ct-bg-brand-gray-light-2 ct-space-x-1 ct-rounded-xl">
                 <div class=""><img src="../assets/tick.png" alt="Up vote"></div>
                 <div class="ct-text-sm">4</div>
             </div>
