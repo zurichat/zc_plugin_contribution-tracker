6494e56f9df53d3f01ef85c12279ae302e7887a2
Jesutooni <edeworuyoyou@gmail.com>
2021-10-01 08:54:16 +0100

    ticket description

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,15 +1,20 @@
 <template>
-  <div>
-    <div class="ct-mb-8">
-      <ComponentHeader />
+  <div class="ct-flex">
+    <div class="ct-flex-grow">
+      <div class="ct-mb-8">
+        <ComponentHeader />
+      </div>
+      <div class="ct-flex ct-justify-end ct-mb-10 ct-mr-3">
+        <AddNewTicket />
+      </div>
+      <OngoingVoting />
+      <TicketList />
+      <TicketCreated />
+      <Archived />
     </div>
-    <div class="ct-flex ct-justify-end ct-mb-10 ct-mr-3">
-      <AddNewTicket />
+    <div>
+      <Description />
     </div>
-    <OngoingVoting />
-    <TicketList />
-    <TicketCreated />
-    <Archived />
   </div>
 </template>
 
@@ -20,9 +25,10 @@ import OngoingVoting from '@/components/OngoingVoting.vue'
 import TicketList from '@/components/TicketList'
 import TicketCreated from '@/components/TicketCreated'
 import Archived from '@/components/Archived'
+import Description from '@/components/DescriptionSection'
 
 export default {
   name: 'Home',
-  components: { ComponentHeader, AddNewTicket, OngoingVoting, TicketList, TicketCreated, Archived }
+  components: { ComponentHeader, AddNewTicket, OngoingVoting, TicketList, TicketCreated, Archived, Description }
 }
 </script>
