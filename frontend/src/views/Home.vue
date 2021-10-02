18df1002ffac15357d0deb4c290ccc72eba00427
Emmanuella Oghenekaro <oghenekaro57@gmail.com>
2021-10-02 23:06:45 +0100

    made some changes

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -4,9 +4,12 @@
       <div class="ct-mb-8">
         <ComponentHeader />
       </div>
+       <!--onclick function to open the add new task form-->
       <div class="ct-flex ct-justify-end ct-mb-8 ct-mr-4">
-        <AddNewTicket />
+        <AddNewTicket @click="() => TogglePopup('buttonTrigger')">
+        </AddNewTicket>
       </div>
+      <FormTicket v-if="popupTriggers.buttonTrigger" :TogglePopup="() => TogglePopup('buttonTrigger')"> </FormTicket>
       <ContributionNavbar />
       <TicketList />
       <TicketCreated />
@@ -21,6 +24,9 @@
 </template>
 
 <script>
+// imported ref and ticket form for the onclick function to work
+import {ref} from 'vue'
+import FormTicket from '@/components/TicketForm.vue'
 import ComponentHeader from '@/components/ComponentHeader'
 import AddNewTicket from '@/components/AddNewTicket'
 import ContributionNavbar from '@/components/ContributionNavbar.vue'
@@ -31,6 +37,20 @@ import Description from '@/components/DescriptionSection'
 
 export default {
   name: 'Home',
-  components: { ComponentHeader, AddNewTicket, ContributionNavbar, TicketList, TicketCreated, Archived, Description }
+  components: { ComponentHeader, AddNewTicket, ContributionNavbar, TicketList, TicketCreated, Archived, Description, FormTicket},
+     // created a function to show the add new ticket form when clicked 
+  setup(){
+    const popupTriggers = ref({
+      buttonTrigger: false
+    });
+    const TogglePopup = (trigger) => {
+       popupTriggers.value[trigger] = !popupTriggers.value[trigger]
+    }
+    return{
+      FormTicket,
+      popupTriggers,
+      TogglePopup 
+    } 
+  }
 }
 </script>
