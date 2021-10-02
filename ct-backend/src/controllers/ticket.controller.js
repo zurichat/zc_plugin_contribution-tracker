6f2be6d986caeffe9383f953599960b7ff084726
trojan782 <davidjacobs522@gmail.com>
2021-10-02 10:34:51 +0100

    added the get a single ticket function

--- ct-backend/src/controllers/ticket.controller.js
+++ ct-backend/src/controllers/ticket.controller.js
@@ -6,59 +6,61 @@ const Ticket = new ZuriDatabase('Tickets')
 
 const ticketController = {
   //create ticket
-	create: async (req, res, next) => {
-		const {
+  create: async (req, res, next) => {
+    const {
 
-		} = req.body
+    } = req.body
 
-		try {
-			const savedRecord = await Ticket.create({
-				//payload goes in here
-			})
-			return Response.send(
-				res,
-				201,
-				savedRecord,
-				'Ticket created successfully'
-			)
-		} catch (error) {
-			return next(error)
-		}
-	},
+    try {
+      const savedRecord = await Ticket.create({
+        //payload goes in here
+      })
+      return Response.send(
+        res,
+        201,
+        savedRecord,
+        'Ticket created successfully'
+      )
+    } catch (error) {
+      return next(error)
+    }
+  },
   //get all tickets
-	fetchAll: async (req, res, next) => {
-		try {
+  fetchAll: async (req, res, next) => {
+    try {
 
-			const { org } = req.query;
-			let data = await Ticket.fetchAll(org)
+      const { org } = req.query;
+      let data = await Ticket.fetchAll(org)
 
-			return Response.send(
-				res,
-				200,
-				data,
-				'Tickets retrieved successfully',
-				true
-			)
-		} catch (err) {
-			return next(err)
-		}
-	},
+      return Response.send(
+        res,
+        200,
+        data,
+        'Tickets retrieved successfully',
+        true
+      )
+    } catch (err) {
+      return next(err)
+    }
+  },
 
   //get a single ticket
-	fetchOne: async (req, res, next) => {
-		try {
+  fetchOne: async (req, res, next) => {
+    try {
       const { org_id } = req.query;
       const { ticket_id } = req.query;
-			const data = await Ticket.fetchOne({ticket: ticket_id}, org_id)
+      const data = await Ticket.fetchOne({
+        ticket: ticket_id
+      }, org_id)
       return Response.send(
         res,
         200,
         data
       )
-		} catch (err) {
-			return next(err)
-		}
-	},
+    } catch (err) {
+      return next(err)
+    }
+  },
 }
 
 export default ticketController;
