6ad64be3a407d21e07b18eb1432bd7eef408e95b
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2021-10-02 15:21:18 -0700

    update to usage of the http-client.js file

--- frontend/src/http-client.js
+++ frontend/src/http-client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: "https://jsonplaceholder.typicode.com",
+    baseURL: "https://jsonplaceholder.typicode.com", // Change the baseURL from jsonplaceholder to the organization URL
     headers: {
         "Content-type": "application/json"
     }
