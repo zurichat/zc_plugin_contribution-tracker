7263f9c1a80e81beae5020646c06c439e55b5c7e
Emmanuella Oghenekaro <oghenekaro57@gmail.com>
2021-10-02 23:06:17 +0100

    created an add new ticket function

--- frontend/src/components/TicketForm.vue
+++ frontend/src/components/TicketForm.vue
@@ -1,31 +1,31 @@
 <template>
-
-    <div class="ct-modal ct-fixed ct-z-50 ct-top-0 ct-left-0 ct-flex ct-items-center ct-justify-center" :class="{'ct-opacity-0': this.$store.state.addUserModalActive,'ct-pointer-events-none': this.$store.state.addUserModalActive, 'ct-modal-active': this.$store.state.addUserModalActive}">
+    <div class="ct-modal ct-fixed ct-z-50 ct-top-0 ct-left-0 ct-flex ct-items-center ct-justify-center" >
         <div class="ct-modal-overlay ct-absolute ct-w-screen ct-h-screen ct-bg-gray-800 ct-opacity-50"></div>
         <div class="ct-form1 ct-bg-white ct-flex ct-flex-col ct-p-4 ct-z-50 ct-rounded ct-shadow-lg z-50">
             <div class="tet3">
-               <h1 class=" hello1  ">Add New Ticket</h1>
-               <div class="ct-cursor-pointer " @click="modalControl">
-                    <button class=" hello2 ct-mr-3 ct-mt-2">&#x2715;</button>
+               <h1 class=" hello1 ">Add New Ticket</h1>
+               <!--on click function to close the add new task form-->
+               <div class="ct-cursor-pointer ">
+                    <button class=" hello2 ct-mr-3 ct-mt-2" @click="TogglePopup">&#x2715;</button>
                </div>
             </div>
-            <form class="ct-w-full ct-px-2 ct-pt-10 ct-flex ct-flex-col" @submit.prevent="AddNewTicket()">
+            <form class="ct-w-full ct-px-2 ct-pt-10 ct-flex ct-flex-col" @submit="AddTicket" method="post">
                 <div class=" text1 ct-mb-50">
                     <div class="ct-mt-3 ct-ml-1">
                 <p class="tet4 ct-text-sm ct-font-bold ct-font-bold">Ticket Name</p>
-                <input type="text" class="tet3 ct-min-w-full ct-h-9 ct-mt-3" >
+                <input type="text" class="tet3 ct-min-w-full ct-h-9 ct-mt-3" name="ticketName" v-model="postTicket.ticketName">
             </div>
             <div class="ct-mt-3 ct-ml-1">
                 <p class="tet4 ct-text-sm ct-font-bold">Description</p>
-                <input type="text" class="tet3 ct-min-w-full ct-h-9 ct-mt-3" >
+                <input type="text" class="tet3 ct-min-w-full ct-h-9 ct-mt-3" name="ticketDes" v-model="postTicket.ticketDes">
             </div>
             <div class="ct-mt-3 ct-ml-1">
                 <p class="tet4 ct-text-sm ct-font-bold">Commit Link</p>
-                <input type="text" class="tet3 ct-min-w-full ct-h-9 ct-mt-3" >
+                <input type="text" class="tet3 ct-min-w-full ct-h-9 ct-mt-3" name="commitLink" v-model="postTicket.commitLink">
             </div>
             <div class="ct-mt-3 ct-ml-1">
                 <p class="tet4 ct-text-sm ct-font-bold">Text Link</p>
-                <input type="text" class="tet3 ct-min-w-full ct-h-9 ct-mt-3" >
+                <input type="text" class="tet3 ct-min-w-full ct-h-9 ct-mt-3" name="textLink" v-model="postTicket.textLink">
 
             </div>
                     
@@ -38,18 +38,38 @@
     </div>
 </template>
 <script>
+// using axios to post the data from the form to the url
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios, axios)
+// script code for the onclick function to close the add new task form
     export default {
         name: 'FormTicket',
-        methods: {
-            modalControl(){
-                this.$store.commit('toggleUserModal');
-            },
+        props: ['TogglePopup'],
+         data(){
+             return{
+                 postTicket:{
+                     ticketName:null,
+                     ticketDes:null,
+                     commitLink:null,
+                     textLink:null
+                 }
+             }
         },
-        computed:{
-        },
+        methods:{
+            AddTicket(e){
+                this.axios.post("", this.postTicket)
+                .then((result)=>{
+                    console.warn(result)
+
+                })
+                e.preventDefault();
+            }
+
+        }
     }
 </script>
-<<<<<<< HEAD
 <style scoped>
 .ct-modal {
   transition: opacity 0.25s ease;
@@ -83,41 +103,4 @@ body.ct-modal-active {
 .tet2:focus{
 border: 1px solid rgb(231, 222, 222);
 }
-=======
-
-<style>
-    .form1{
-        position: relative;
-        height: 419px;
-        width: 300px;
-        border-radius: 4px;
-        background: rgba(255, 255, 255, 1);
-        box-shadow: 1px 1px 10px rgba(228, 225, 225, 0.5);
-    }
-    .tet1{
-        font-family: Lato;
-        font-style: normal;
-        font-weight: bold;
-        line-height: 34px;
-    }
-    .tet2{
-        margin-left: 140px;
-    }
-    .tet2:focus{
-        border: 1px solid rgb(231, 222, 222);
-    }
-    .tet3{
-        border: 1px solid  rgb(231, 222, 222);
-        border-radius: 3px;
-    }
-    .tet4{
-        color: rgba(29, 28, 29, 1);
-        font-style: normal;
-    }
-    .tet5{
-        background: #00B87C;
-        border-radius: 3px;
-        margin-left: 100px;
-    }
->>>>>>> 78bc4bc956bec8310bf2a15d7373fe03cc0f3a1b
 </style>
