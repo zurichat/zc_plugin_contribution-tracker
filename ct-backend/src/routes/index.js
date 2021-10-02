26f1a883125b5f9907c948ca896abcb2e92d87f7
Adams Lee <officialman776@gmail.com>
2021-10-02 09:34:22 +0100

    added update test url endpoint

--- ct-backend/src/routes/index.js
+++ ct-backend/src/routes/index.js
@@ -4,11 +4,13 @@ import ticketController from "../controllers/ticket.controller";
 const indexRouter = express.Router();
 
 indexRouter.get("/", pluginInfoController.getPluginInfo);
-
+// update status
 indexRouter.put("/tickets/status", ticketController.updateStatus);
-
+// upvote
 indexRouter.put("/tickets/upvote", ticketController.doUpvote);
-
+//downvote
+indexRouter.put("/tickets/downvote", ticketController.doDownvote);
+// update test url
 indexRouter.put("/tickets/downvote", ticketController.doDownvote);
 
 export default indexRouter;
