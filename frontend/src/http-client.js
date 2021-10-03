ff386c880bf57c0246ab2ec27ef0d210665e33d6
Boye <rippyblogger@gmail.com>
2021-10-03 11:32:20 +0400

    Setup Centrifuge, made minor additions to axios file

--- frontend/src/http-client.js
+++ frontend/src/http-client.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: "https://jsonplaceholder.typicode.com", // Change the baseURL from jsonplaceholder to the organization URL
+    baseURL: "https://ct.zuri.chat/api/v1/", // Change the baseURL from jsonplaceholder to the organization URL
     headers: {
+        Accept: 'application/json',
         "Content-type": "application/json"
-    }
+    },
+    timeout: 300000
 })
+
+
