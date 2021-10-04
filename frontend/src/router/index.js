ac3cd52199828b94bf1dfa2be97798f569d2efc9
unknown <izik4004@gmail.com>
2021-10-04 10:27:58 +0100

    completed spa setup

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -31,14 +31,14 @@ const routes = [
     name: 'RequestWork',
     component: RequestWork
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const router = createRouter({
