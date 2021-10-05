d127d58e963943b2a6d461c3852a37dd28ac44ed
airondev <airondev@gmail.com>
2021-10-05 12:29:21 +0100

    Endpoint to fetch org member with added ct-role info

--- ct-backend/src/controllers/organization.controller.js
+++ ct-backend/src/controllers/organization.controller.js
@@ -2,8 +2,11 @@
 import Response from '../utils/response'
 import CustomError from "../utils/custom-error"
 import ZuriOrganization from "../zuricore/organization"
+import ZuriDatabase from "../zuricore/db"
+
 
 const Organization = new ZuriOrganization();
+const Voter = new ZuriDatabase("ct_voters");
 
 const OrganizationController = {
 
@@ -22,6 +25,55 @@ const OrganizationController = {
       next(error)
     }
   },
+
+
+  async getMember(req, res, next) {
+    try {
+
+      const { org_id, member_id } = req.params
+     
+      const bearerHeader = req.headers['authorization'];
+
+      const bearer = bearerHeader.split(' ');
+      const bearerToken = bearer[1];
+
+      const member = await Organization.getMember(org_id, member_id, bearerToken);
+      
+      if(member){
+        const user_name  = member.user_name
+        const voters = await Voter.fetchAll(org_id)
+        switch(member.role) {
+          case "owner":
+            member.ct_admin = true
+            break;
+          case "admin":
+            member.ct_admin = true
+            break;
+          default:
+           member.ct_admin = false
+        }
+        voters.forEach((voter)=>{
+          if(voter.user_name == user_name){
+            return member.isVoter = true;
+          }else{
+            return member.ct_voter = false;
+          }
+        })
+      }
+
+      // const voters = this.isVoter(member, org_id)
+      return Response.send(
+        res,
+        200,
+        member,
+        "Member retrieved successfully"
+      )
+    } catch (error) {
+      next(error)
+    }
+  },
+
+
 }
 
 export default OrganizationController
--- ct-backend/src/routes/organization.route.js
+++ ct-backend/src/routes/organization.route.js
@@ -7,6 +7,9 @@ import { userOrg } from "../middlewares/check_org.middleware"
 // retrieve org members from zuri core
 organizationRouter.get("/:org_id/members", OrganizationController.getMembers);
 
+// retrieve a org members from zuri core
+organizationRouter.get("/:org_id/members/:member_id", OrganizationController.getMember);
+
 
 // Export module
 export default organizationRouter
