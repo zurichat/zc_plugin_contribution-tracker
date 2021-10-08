c24f39406ca0ad378b1654f8540d85258e2085a2
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2021-10-08 10:03:43 -0700

    Update of the views

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,7 +3,6 @@ import Home from '../views/Home.vue'
 import Admin from '../views/Admin.vue'
 import RequestFeature from '../views/RequestFeature.vue'
 import OngoingVoting from '../views/OngoingVoting.vue'
-import RequestWork from '../views/RequestWork.vue'
 
 const routes = [
   {
@@ -31,6 +30,14 @@ const routes = [
     name: 'RequestWork',
     component: RequestWork
   },
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const router = createRouter({
